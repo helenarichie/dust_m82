a213404b6ab769a291117e5d4386bdcc1b4be323
helenarichie <helenarichie@gmail.com>
2024-09-06 18:01:15 -0400

    start writing log parser (incomplete)

--- parse/parse_log.py
+++ parse/parse_log.py
@@ -3,7 +3,7 @@ sys.path.insert(0, "/ix/eschneider/helena/code/my_scripts/")
 from hconfig import *
 from csv import writer
 
-date = "2024-07-07"
+date = "m82"
 
 ###############################
 crc = True
@@ -13,8 +13,8 @@ frontier = False
 ########## data type ############
 debugging = False
 cloud_wind = False
-testing = False
-m82 = True
+testing = True
+m82 = False
 #################################
 
 if crc:
@@ -31,73 +31,51 @@ if frontier:
 
 csvdir = os.path.join(basedir, "csv/")
 
-f = open(os.path.join(csvdir, "sputter_hot.csv"), "w")
+f = open(os.path.join(csvdir, "dust_0.csv"), "w")
 f.close()
-f = open(os.path.join(csvdir, "sputter.csv"), "w")
+f = open(os.path.join(csvdir, "dust_1.csv"), "w")
 f.close()
-f = open(os.path.join(csvdir, "mass_cloud.csv"), "w")
+f = open(os.path.join(csvdir, "dust_2.csv"), "w")
 f.close()
-f = open(os.path.join(csvdir, "mass_dust.csv"), "w")
+f = open(os.path.join(csvdir, "dust_3.csv"), "w")
 f.close()
-f = open(os.path.join(csvdir, "time.csv"), "w")
+f = open(os.path.join(csvdir, "gas.csv"), "w")
 f.close()
 
-sputter_hot, sputter = [], []
-total_sputter = [sputter, sputter_hot]
-strings_sputter = ["Mixed sputtered mass: ", "Hot sputtered mass: "]
-mass_cloud, mass_dust = [], []
-total_mass = [mass_cloud, mass_dust]
-strings_mass = ["Cloud mass: ", "Dust mass: "]
 times = []
-
-i = 0
-l = 0
-m = 0
+times_counter = 0
 
 with open(os.path.join(basedir, "output.log")) as f:
     for line in f:
-        if line.startswith("** "):
-            line = line.lstrip("** ").rstrip(" \n")
-            line = line.split("  ")
-            for j, entry in enumerate(line):
-                entry = entry.lstrip(strings_sputter[j])
-                total_sputter[j].append(float(entry))
-                if j == 0:
-                    with open(os.path.join(csvdir, "sputter.csv"), "a") as f_txt:
-                        writer_obj = writer(f_txt)
-                        writer_obj.writerow([total_sputter[j][i]])
-                        f_txt.close()
-                if j == 1:
-                    with open(os.path.join(csvdir, "sputter_hot.csv"), "a") as f_txt:
-                        writer_obj = writer(f_txt)
-                        writer_obj.writerow([total_sputter[j][i]])
-                        f_txt.close()
-            i += 1
-
-        elif line.startswith("@@ "):
-            line = line.lstrip("@@ ").rstrip(" \n")
-            line = line.split("  ")
-            for j, entry in enumerate(line):
-                entry = entry.lstrip(strings_mass[j])
-                total_mass[j].append(float(entry))
-                if j == 0:
-                    with open(os.path.join(csvdir, "mass_cloud.csv"), "a") as f_txt:
-                        writer_obj = writer(f_txt)
-                        writer_obj.writerow([total_mass[j][l]])
-                        f_txt.close()
-                if j == 1:
-                    with open(os.path.join(csvdir, "mass_dust.csv"), "a") as f_txt:
-                        writer_obj = writer(f_txt)
-                        writer_obj.writerow([total_mass[j][l]])
-                        f_txt.close()
-            l += 1
-
+        line_split = list(line)
+        row_dust_0 = np.zeros((11, 3))
+        row_dust_1 = np.zeros((11, 3))
+        row_dust_2 = np.zeros((11, 3))
+        row_dust_3 = np.zeros((11, 3))
+        row_gas = np.zeros((11, 3))
+        row_dust_0[:] = -1
+        if line_split[0].isdigit():
+            if line_split[3] == "S":
+                line = line.replace("Sputtered mass:  (hot) ", "")
+                line = line.replace("(mixed) ", "")
+                line = line.replace("(cool) ", "")
+                line.rstrip("\n")
+                line = line.split("  ")
+                for i in range(0, 10):
+                    if line[0] == str(i):
+                        row_dust_0[i+1] = [float(line[1]), float(line[5]), float(line[9])]
+                        row_dust_1[i+1] = [float(line[2]), float(line[6]), float(line[10])]
+                        row_dust_2[i+1] = [float(line[3]), float(line[7]), float(line[11])]
+                        row_dust_3[i+1] = [float(line[4]), float(line[8]), float(line[12])]
         elif line.startswith("n_step"):
             line = line.split(" ")
             times.append(float(line[6]))
+            row_dust_0[0] = [float(line[6]), float(line[6]), float(line[6])]
+            row_dust_1[0] = [float(line[6]), float(line[6]), float(line[6])]
+            row_dust_2[0] = [float(line[6]), float(line[6]), float(line[6])]
+            row_dust_3[0] = [float(line[6]), float(line[6]), float(line[6])]
             with open(os.path.join(csvdir, "time.csv"), "a") as f_txt:
                 writer_obj = writer(f_txt)
-                writer_obj.writerow([times[m]])
+                writer_obj.writerow([times[times_counter]])
+                times_counter += 1
                 f_txt.close()
-
-            m += 1
