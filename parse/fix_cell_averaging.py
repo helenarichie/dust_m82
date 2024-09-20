44cc3d003e6b25df4ac31e9a810bde75949c1367
helenarichie <helenarichie@gmail.com>
2024-09-20 11:48:40 -0400

    clean up script

--- parse/fix_cell_averaging.py
+++ parse/fix_cell_averaging.py
@@ -3,7 +3,7 @@ sys.path.insert(0, "/ix/eschneider/helena/code/my_scripts/")
 from hconfig import *
 from csv import writer
 
-date = "2024-08-28"
+date = "2024-08-29"
 
 ###############################
 crc = True
@@ -31,49 +31,17 @@ if frontier:
 
 csvdir = os.path.join(basedir, "csv/")
 
-f = open(os.path.join(csvdir, "sputtered_0.csv"), "w")
-f.close()
-f = open(os.path.join(csvdir, "sputtered_1.csv"), "w")
-f.close()
-f = open(os.path.join(csvdir, "sputtered_2.csv"), "w")
-f.close()
-f = open(os.path.join(csvdir, "sputtered_3.csv"), "w")
-f.close()
-f = open(os.path.join(csvdir, "dust_0.csv"), "w")
-f.close()
-f = open(os.path.join(csvdir, "dust_1.csv"), "w")
-f.close()
-f = open(os.path.join(csvdir, "dust_2.csv"), "w")
-f.close()
-f = open(os.path.join(csvdir, "dust_3.csv"), "w")
-f.close()
-f = open(os.path.join(csvdir, "gas.csv"), "w")
+f = open(os.path.join(csvdir, "high_z_fixed.log"), "w")
 f.close()
 
-times = []
-times_counter = 0
-
-row_sputtered_0 = np.zeros((10, 4))
-row_sputtered_1 = np.zeros((10, 4))
-row_sputtered_2 = np.zeros((10, 4))
-row_sputtered_3 = np.zeros((10, 4))
-row_dust_0 = np.zeros((10, 4))
-row_dust_1 = np.zeros((10, 4))
-row_dust_2 = np.zeros((10, 4))
-row_dust_3 = np.zeros((10, 4))
-row_gas = np.zeros((10, 4))
-
-f = open(os.path.join(csvdir, "m82_fixed.log"), "w")
-f.close()
-
-with open(os.path.join("m82_output_copy.log")) as f:
+with open(os.path.join(basedir, "output.log")) as f:
     sub_str = "Average"
     counter = 0
     for line in f:
         # print(line)
         if sub_str in line:
             line = line[:line.find(sub_str)]
-            with open(os.path.join(csvdir, "m82_fixed.log"), "a") as f_txt:
+            with open(os.path.join(csvdir, "high_z_fixed.log"), "a") as f_txt:
                     f_txt.write(line)
                     f_txt.close()
             counter = 2
@@ -81,6 +49,6 @@ with open(os.path.join("m82_output_copy.log")) as f:
         if counter != 0:
             counter -= 1
             continue
-        with open(os.path.join(csvdir, "m82_fixed.log"), "a") as f_txt:
+        with open(os.path.join(csvdir, "high_z_fixed.log"), "a") as f_txt:
                 f_txt.write(line.lstrip())
                 f_txt.close()
