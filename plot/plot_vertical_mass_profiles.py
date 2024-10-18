ce4fd7a65764196281fbf6a93dc3b4e2fb26b63e
helenarichie <helenarichie@gmail.com>
2024-10-18 14:31:58 -0400

    slight improvements and clean up

--- plot/plot_vertical_mass_profiles.py
+++ plot/plot_vertical_mass_profiles.py
@@ -12,13 +12,15 @@ if mypc:
 from hconfig import *
 
 date = "2024-08-28"
-tmaxs = np.linspace(0, 51) * 1e3
+tmaxs = np.linspace(0, 50000, 101)
+
 substr = "gas"
 # ymax = 3e8  # high-z gas
-ymax = 2e7  # m82 gas
+ymax = 4e7  # m82 gas
 # ymax = 3e6  # high-z dust
 # ymax = 2e4  # m82 dust
 no_disk = True
+disk_i = 1
 
 ########## data type ############
 debugging = False
@@ -43,11 +45,11 @@ if mypc:
 
 csvdir = os.path.join(basedir, "csv/")
 
-masses = np.zeros((10, 3))
+
+d_arr = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 if no_disk:
-    d_arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-else:
-    d_arr = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    d_arr = d_arr[disk_i:]
+
 labels = ["hot", "mixed", "cool"]
 
 breakout = False
@@ -55,42 +57,42 @@ tmax_i = 0
 with open(os.path.join(csvdir, f"{substr}.csv")) as f:
     for line in f:
         line = line.split(",")
+        masses = np.zeros((10, 3))
         for i, bin in enumerate(line):
             bin = bin.replace("[", "")
             bin = bin.replace("]", "")
             bin = bin.split(" ")
             while("" in bin):
                 bin.remove("")
-            if i < 10:
-                masses[i] = np.array(bin[1:4], dtype=float)
-            if float(bin[0]) <= tmaxs[tmax_i]:
-                continue
+            if i < 10:  # should be unneccessary, I should be 0-9, just checking there there's less than 10 bins
+                masses[i] = np.array(bin[1:4], dtype=float)  # 1:4 = hot, mixed, cold (excludes 0, which is time)
+        if float(bin[0]) == tmaxs[tmax_i]:
+            for j in range(0, 2+1):
+                if no_disk:
+                    plt.stairs(masses[:,j][disk_i:], d_arr, label=labels[j] + rf", total = {np.sum(masses[:,j][disk_i:]):.1e} $M_\odot$", linewidth=2)
+                    print(f"{labels[j]} {substr} total mass at {round(tmaxs[tmax_i]/1e3, 1):.2e} Myr: {np.sum(masses[:,j][disk_i:]):.1e} M_sun")
+                else:
+                    plt.stairs(masses[:,j], d_arr, label=labels[j] + rf", total = {np.sum(masses[:,j]):.1e} $M_\odot$", linewidth=2)
+            print("\n")
+            plt.title(f"{substr} mass, $t={round(tmaxs[tmax_i]/1e3, 1)}$ Myr")
+            plt.yscale('log')
+            plt.ylabel(r"$log_{10}(M~[M_\odot])$")
+            plt.xlabel(r"$r~[kpc]$")
+            plt.ylim(10, ymax)
+            plt.legend()
+            plt.tick_params(top=True, right=True, which="both")
+            if mypc:
+                plt.savefig(os.path.join(basedir, "no_disk", substr, f"{int(tmaxs[tmax_i]/500)}_{substr}.png"), dpi=300)
             else:
-                for j in range(0, 2+1):
-                    masses_arr = np.array(masses[:,j])
-                    if no_disk:
-                        plt.stairs(masses_arr[1:], d_arr, label=labels[j] + rf", total = {np.sum(masses[:,j][1:]):.1e} $M_\odot$", linewidth=2)
-                    else:
-                        plt.stairs(masses_arr, d_arr, label=labels[j] + rf", total = {np.sum(masses[:,j][1:]):.1e} $M_\odot$", linewidth=2)
-                    print(f"{labels[j]} {substr} total mass at {round(tmaxs[tmax_i]/1e3, 1):.2e} Myr: {np.sum(masses[:,j][1:]):.2e} M_sun")
-                print("\n")
-                plt.title(f"{substr} mass, $t={round(tmaxs[tmax_i]/1e3, 1)}$ Myr")
-                plt.yscale('log')
-                plt.ylabel(r"$log_{10}(M~[M_\odot])$")
-                plt.xlabel(r"$r~[kpc]$")
-                plt.ylim(10, ymax)
-                plt.legend()
-                plt.tick_params(top=True, right=True, which="both")
-                if mypc:
-                    plt.savefig(os.path.join(basedir, "no_disk", substr, f"{int(tmaxs[tmax_i]/1e3)}_{substr}.png"), dpi=300)
-                else:
-                    plt.savefig(f"../png/{substr}/{int(tmaxs[tmax_i]/1e3)}_{substr}.png", dpi=300)
-                plt.close()
+                plt.savefig(f"../png/{substr}/{int(tmaxs[tmax_i]/1e3)/500}_{substr}.png", dpi=300)
+            plt.close()
 
-                if tmax_i < (len(tmaxs)-1):
-                    tmax_i += 1
-                else:
-                    breakout = True
-                    break
+            if tmax_i < (len(tmaxs)-1):
+                tmax_i += 1
+            # if you're at the end of the tmaxs array, break out of the loop and end the program
+            else:
+                breakout = True
+                break
+        
         if breakout:
             break
