11c4ac4874d7f4a82e2d71528d90cf43ed0ab577
helenarichie <helenarichie@gmail.com>
2025-01-03 11:41:57 -0500

    update print statement

--- plot/plot_vertical_sputtering_profiles.py
+++ plot/plot_vertical_sputtering_profiles.py
@@ -50,14 +50,15 @@ def main(basedir, field_name, exclude_disk, ymax, mode):
                 if i < 10:
                     sputtered[i] = sputtered[i] + np.array(bin[1:4], dtype=float)
                 if float(bin[0]) == tmaxs[tmax_i]:
+                    print(f"t = {round(tmaxs[tmax_i]/1e3, 1):.2e} Myr")
                     for j, color in enumerate(colors):
                         plt.stairs(sputtered[:,j][1:], d_arr, color=color, label=labels[j] + rf", total = {np.sum(sputtered[:,j][1:]):.1e} $M_\odot$", linewidth=2, zorder=-j)
-                        print(f"{labels[j]} {field_name} total mass at {round(tmaxs[tmax_i]/1e3, 1):.2e} Myr: {np.sum(sputtered[:,j][1:]):.2e} M_sun")
+                        print(f"{labels[j]} {field_name} cumulative mass: {np.sum(sputtered[:,j][1:]):.2e} M_sun")
                     print("\n")
                     plt.title(f"cumulative {field_name} mass, $t={round(tmaxs[tmax_i]/1e3, 1)}$ Myr")
                     plt.yscale('log')
-                    plt.ylabel(r"$log_{10}(M~[M_\odot])$")
-                    plt.xlabel(r"$r~[kpc]$")
+                    plt.ylabel(r"$log_{10}(m~[M_\odot])$")
+                    plt.xlabel(r"$z~[kpc]$")
                     plt.ylim(1, ymax)
                     plt.legend()
                     plt.savefig(os.path.join(basedir, "png", field_name, f"{int(tmaxs[tmax_i]/500)}_{field_name}.png"), dpi=300)
