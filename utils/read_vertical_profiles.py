a29b9076eda150bfdd9aa5aacf01bf6fd16e662a
helenarichie <helenarichie@gmail.com>
2025-03-04 11:13:06 -0500

    add script to read csv files with dust/gas/sputtered mass data

--- utils/read_vertical_profiles.py
+++ utils/read_vertical_profiles.py
@@ -2,18 +2,15 @@ import os
 import numpy as np
 
 def read_vertical_profiles(csvdir, field_names):
-    times = None
     mass_hot = []
     mass_mixed = []
     mass_cool = []
     for field in field_names:
-        times = []
         mass_i = []
         with open(os.path.join(csvdir, f"{field}_hot_short.csv")) as f:
             for line in f:
                 line = line.split(",")
-                mass_i.append(np.array(line[1:], dtype=float))
-                times.append(float(line[0]))
+                mass_i.append(np.array(line[0:], dtype=float))
         mass_hot.append(mass_i)
         f.close()
 
@@ -21,7 +18,7 @@ def read_vertical_profiles(csvdir, field_names):
         with open(os.path.join(csvdir, f"{field}_mixed_short.csv")) as f:
             for line in f:
                 line = line.split(",")
-                mass_i.append(np.array(line[1:], dtype=float))
+                mass_i.append(np.array(line[0:], dtype=float))
         mass_mixed.append(mass_i)
         f.close()
 
@@ -29,12 +26,21 @@ def read_vertical_profiles(csvdir, field_names):
         with open(os.path.join(csvdir, f"{field}_cool_short.csv")) as f:
             for line in f:
                 line = line.split(",")
-                mass_i.append(np.array(line[1:], dtype=float))
+                mass_i.append(np.array(line[0:], dtype=float))
         mass_cool.append(mass_i)
         f.close()
+
+    times = []
+    with open(os.path.join(csvdir, f"time_short.csv")) as f:
+        for line in f:
+            line = line.split(",")
+            times.append(float(line[0]))
+    mass_cool.append(mass_i)
+    f.close()
+
     mass_hot = np.array(mass_hot)
     mass_mixed = np.array(mass_mixed)
-    times = np.array(times)
     mass_cool = np.array(mass_cool)
+    times = np.array(times)
     
     return times, mass_hot, mass_mixed, mass_cool
